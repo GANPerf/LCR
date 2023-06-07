0eeab1a258af629baa9d0ad6fbd36745911b15cd
Yangyang Shu <50609620+GANPerf@users.noreply.github.com>
2023-06-07 11:39:31 +0930

    Update __init__.py

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -20,6 +20,8 @@ def get_optimizer(name, model, lr, momentum, weight_decay):
         optimizer = LARS(parameters, lr=lr, momentum=momentum, weight_decay=weight_decay)
     elif name == 'sgd':
         optimizer = torch.optim.SGD(parameters, lr=lr, momentum=momentum, weight_decay=weight_decay)
+    elif name == 'lars_simclr': # Careful
+        optimizer = LARS_simclr(model.named_modules(), lr=lr, momentum=momentum, weight_decay=weight_decay)
     elif name == 'larc':
         optimizer = LARC(
             torch.optim.SGD(
